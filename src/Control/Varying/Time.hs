bbf7ed4e461d90c31210c4c2ff6da724cdca9c5a
Schell Scivally <efsubenovex@gmail.com>
2015-08-13 11:37:03 -0700

    Applicative additions for ghc 7.8

--- src/Control/Varying/Time.hs
+++ src/Control/Varying/Time.hs
@@ -16,7 +16,8 @@ deltaUTC = delta getCurrentTime (\a b -> realToFrac $ diffUTCTime a b)
 
 -- | Produces "time" deltas using a monadic computation and a difference
 -- function.
-delta :: (Num t, Fractional t, Monad m) => m a -> (a -> a -> t) -> Var m b t
+delta :: (Num t, Fractional t, Applicative m, Monad m)
+      => m a -> (a -> a -> t) -> Var m b t
 delta m f = Var $ \_ -> do
     t <- m
     return (0, delta' t)
@@ -30,7 +31,7 @@ delta m f = Var $ \_ -> do
 -- | Emits events before accumulating t of input dt.
 -- Note that as soon as we have accumulated >= t we stop emitting events
 -- and there is no guarantee that an event will be emitted at time == t.
-before :: (Monad m, Num t, Ord t) => t -> Var m t (Event ())
+before :: (Applicative m, Monad m, Num t, Ord t) => t -> Var m t (Event ())
 before t = Var $ \dt -> do
     if t - dt >= 0
     then return (Event (), before $ t - dt)
@@ -39,7 +40,7 @@ before t = Var $ \dt -> do
 -- | Emits events after t input has been accumulated.
 -- Note that event emission is not guaranteed to begin exactly at t,
 -- only at some small delta after t.
-after :: (Monad m, Num t, Ord t) => t -> Var m t (Event ())
+after :: (Applicative m, Monad m, Num t, Ord t) => t -> Var m t (Event ())
 after t = Var $ \dt -> do
     if t - dt <= 0
     then return (Event (), pure $ Event ())
