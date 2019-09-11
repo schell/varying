3081477d71a2f1e755718ad8b0eef21695ec2fac
Schell Scivally <efsubenovex@gmail.com>
2019-09-11 09:37:14 -0700

    better docs for tween, cicd

---
+++ ci.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+export DIR=`pwd`
+export PROJECT_DIR="."
+
+# get ready to build the project
+prebuild () {
+  echo "Doing prebuild stuff (installing with apt, getting stack, etc...)"
+  apt-get update -y
+  apt-get install -y wget libtinfo-dev git
+  cd $PROJECT_DIR
+  wget -qO- https://get.haskellstack.org/ | sh
+  export STACK_ROOT=`pwd`/.stack
+  stack setup
+  cd $DIR
+}
+
+
+# build the project
+build () {
+  stack install --only-dependencies
+  stack build || exit 1
+  stack test || exit 1
+  stack bench
+}
+
+
+deploy () {
+  echo "Deploying..."
+  prebuild
+  stack build
+  stack sdist .
+  stack upload --no-signature .
+}
